aa1c5fd266aebe074db0e7255a29039e96fb3a29
Yash kandiyal <yashkandiyal22@gmail.com>
2024-05-09 23:30:24 +0530

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,62 @@ import TodoStore from "./TodoStore";
 
 const App = () => {
   const [inputTodo, setInputTodo] = useState<string>("");
-  const { addTodo, todos, deleteTodo } = TodoStore((state) => state);
+  const { addTodo, todos, deleteTodo, toggleStatus } = TodoStore(
+    (state) => state
+  );
+
   const AddToDo = () => {
     addTodo(inputTodo);
-    setInputTodo(""); // Clear input after adding todo
+    setInputTodo("");
   };
-  const deletetingTodo = (id: number) => {
-    deleteTodo(id);
+
+  const handleDeleteTodo = (index: number) => {
+    deleteTodo(index);
   };
+
+  const handleToggleStatus = (index: number) => {
+    toggleStatus(index);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <input
-        type="text"
-        className="border border-gray-300 rounded px-4 py-2 mb-4"
-        placeholder="Enter your todo"
-        value={inputTodo}
-        onChange={(e) => setInputTodo(e.target.value)}
-      />
-      <button
-        className="bg-blue-500 text-white font-semibold px-4 py-2 rounded"
-        onClick={AddToDo}>
-        Add Todo
-      </button>
-      {/* Display todos */}
-      <div className="mt-4">
-        {todos?.map((todo, index) => (
-          <div
-            key={index}
-            className="bg-gray-100 rounded px-6 py-2 mb-2 flex gap-2 justify-between items-center">
-            {todo}
+      <div className="w-80">
+        <input
+          type="text"
+          className="border border-gray-300 rounded px-4 py-2 mb-4 w-full"
+          placeholder="Enter your todo"
+          value={inputTodo}
+          onChange={(e) => setInputTodo(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white font-semibold px-4 py-2 rounded w-full"
+          onClick={AddToDo}>
+          Add Todo
+        </button>
+        {/* Display todos */}
+        <div className="mt-4">
+          {todos?.map((todo, index) => (
             <div
-              onClick={() => deletetingTodo(index)}
-              className="bg-red-500 p-1">
-              delete
+              key={index}
+              className="bg-gray-100 rounded px-4 py-2 mb-2 flex justify-between items-center">
+              <input
+                type="checkbox"
+                className="form-checkbox h-5 w-5 text-blue-600"
+                checked={todo.completed}
+                placeholder="status"
+                onChange={() => handleToggleStatus(index)}
+              />
+              <span className={`ml-2 ${todo.completed ? "line-through" : ""}`}>
+                {todo.text}
+              </span>
+              <button
+                onClick={() => handleDeleteTodo(index)}
+                className="text-red-600">
+                delete
+              </button>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/TodoStore.ts
+++ src/TodoStore.ts
@@ -1,17 +1,30 @@
 import { create } from "zustand";
 
 type Todos = {
-  todos: string[];
+  todos: { text: string; completed: boolean }[];
   addTodo: (todo: string) => void;
   deleteTodo: (index: number) => void;
+  toggleStatus: (index: number) => void;
 };
 
 const TodoStore = create<Todos>((set) => ({
+
   todos: [],
-  addTodo: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
+  
+  addTodo: (todo) =>
+    set((state) => ({
+      
+       todos: [...state.todos, { text: todo, completed: false }],
+    })),
   deleteTodo: (index) =>
     set((state) => ({
       todos: state.todos.filter((_, i) => i !== index),
+    })),
+  toggleStatus: (index) =>
+    set((state) => ({
+      todos: state.todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      ),
     })),
 }));
 
